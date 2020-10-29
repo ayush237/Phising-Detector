344bb21ffeb81dcd78d3ea7aefcd594d1f578466
dhruvstan <50947002+dhruvstan@users.noreply.github.com>
2020-10-30 01:27:35 +0530

    added templates and web deployment

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from wsgiref import simple_server
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask import Response
 import os
 from flask_cors import CORS, cross_origin
 from prediction_Validation_Insertion import pred_validation
 from trainingModel import trainModel
 from training_Validation_Insertion import train_validation
-import flask_monitoringdashboard as dashboard
+from file_operations import file_methods
+from application_logging import logger
+import numpy as np
+
+#import flask_monitoringdashboard as dashboard
 from predictFromModel import prediction
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
-dashboard.bind(app)
+#dashboard.bind(app)
 CORS(app)
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+
 
 
 
@@ -22,8 +32,20 @@ CORS(app)
 @cross_origin()
 def predictRouteClient():
     try:
-        if request.json['folderPath'] is not None:
-            path = request.json['folderPath']
+        if request.json is not None:
+            path = request.json['filepath']
+
+            pred_val = pred_validation(path) #object initialization
+
+            pred_val.prediction_validation() #calling the prediction_validation function
+
+            pred = prediction(path) #object initialization
+
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return Response("Prediction File created at %s!!!" % path)
+        elif request.form is not None:
+            path = request.form['filepath']
 
             pred_val = pred_validation(path) #object initialization
 
@@ -49,8 +71,8 @@ def predictRouteClient():
 def trainRouteClient():
 
     try:
-        if request.json['folderPath'] is not None:
-            path = request.json['folderPath']
+        if request.json['filepath'] is not None:
+            path = request.json['filepath']
             train_valObj = train_validation(path) #object initialization
 
             train_valObj.train_validation()#calling the training_validation function
@@ -73,10 +95,6 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successfull!!")
 
-port = int(os.getenv("PORT"))
+port = int(os.getenv("PORT",5001))
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    port = 5000
-    httpd = simple_server.make_server(host, port, app)
-    print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    app.run(port=port,debug=True)
